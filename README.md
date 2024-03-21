231894d8bc52807db2e48e6f584eb921e59c2040
BirchFst <142733718+BirchFst@users.noreply.github.com>
2024-03-21 21:16:44 +0800

    upload
    upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Auto download and rename
+
+This is personal project, but if you need to use it, welcome to clone to your computer!
---
+++ download.py
@@ -0,0 +1,18 @@
+import wget
+import time
+import os
+
+if not os.path.exists("./download"):
+    os.mkdir("./download")
+
+if not os.path.exists("./url.txt"):
+    open("./url.txt",'w').close()
+
+    print("Put your url in the url.txt file and restart!")
+
+else:
+    urls = open("./url.txt").readlines()
+
+    for url in urls:
+        print("Start downloading...")
+        wget.download(url,out="./download")
